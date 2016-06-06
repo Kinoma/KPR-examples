0c9d00a0a7e65a0d9b1753b680512e502842fee3
wincelet <brookelfnichols@gmail.com>
2016-06-06 14:38:32 -0700

    Fix space/tabs issue

--- simplesynth/src/main.js
+++ simplesynth/src/main.js
@@ -44,22 +44,22 @@ var errorStyle = new Style({ font:"bold 40px", color:"white", horizontal:"center
 // ---------------------------------------------------------------------------------------------------------
 // White keys template
 var Key = Content.template(function($) { return {
-    top: 2, width: 38, height: 150, skin: KeySkin,
-    behavior: Behavior({
-        onCreate: function(container, data) {
-            this.frequency = data;
-        },
-    }),
+	top: 2, width: 38, height: 150, skin: KeySkin,
+	behavior: Behavior({
+		onCreate: function(container, data) {
+			this.frequency = data;
+		},
+	}),
 }});
 
 // Black keys template
 var HalfKey = Content.template(function($) { return {
-    top: 2, width: 18, height: 90, skin: HalfKeySkin,
-    behavior: Behavior({
-        onCreate: function(container, data) {
-            this.frequency = data;
-        }, 
-    }),
+	top: 2, width: 18, height: 90, skin: HalfKeySkin,
+	behavior: Behavior({
+		onCreate: function(container, data) {
+			this.frequency = data;
+		}, 
+	}),
 }});
 
 // Synth mode buttons template 
@@ -79,144 +79,137 @@ var ButtonTray = Container.template(function($) { return {
 		// Calculate index of button of touched based on x-y
 		hitIndex: function(container, x, y) {
 			y -= container.y;
-            if ((0 <= y) && (y < 80)) {     	
+			if ((0 <= y) && (y < 80)) {     	
 				x -= container.x;
 				return Math.floor(x / 80);	            
-            } else return -1;
-        },
-        // Determines current touch index, compares to current, switches modes accordingly
-        hitTouch: function(container, id, current) {
-            var former = this.touches;
-            if (former != current && current != -1) {
-            	if(former >=0)
-                	container.content(former).state = 0;               
-                if(current>=0)
-                	container.content(current).state = 1;
-                	if(current == 0) synthoption = "";
-                	if(current == 1) synthoption = "violin";
-                	if(current == 2) synthoption = "bell";
-                	if(current == 3) synthoption = "laser";
-                this.touches = current;           
-            }	
-        },
-        // Initialize touch value -> default synth option
-        onCreate: function(container, data) {
-            this.touches = 0;
-            container.first.state = 1;
-            synthoption = "";
-        },
-        // Fires on a touch
-        onTouchBegan: function(container, id, x, y, tick) {
-            this.hitTouch(container, id, this.hitIndex(container, x, y));
-        },		
-    }),
-    // Map synthesizer mode buttons
-    contents: $.map(function(name, index) {
-    	return new SynthButton(name, {left: index*80});     	
+			} else return -1;
+		},
+		// Determines current touch index, compares to current, switches modes accordingly
+		hitTouch: function(container, id, current) {
+			var former = this.touches;
+			if (former != current && current != -1) {
+				if(former >= 0) container.content(former).state = 0;               
+				if(current >= 0) container.content(current).state = 1;
+				if(current == 0) synthoption = "";
+				if(current == 1) synthoption = "violin";
+				if(current == 2) synthoption = "bell";
+				if(current == 3) synthoption = "laser";
+				this.touches = current;           
+			}	
+		},
+		// Initialize touch value -> default synth option
+		onCreate: function(container, data) {
+			this.touches = 0;
+			container.first.state = 1;
+			synthoption = "";
+		},
+		// Fires on a touch
+		onTouchBegan: function(container, id, x, y, tick) {
+			this.hitTouch(container, id, this.hitIndex(container, x, y));
+		},		
+	}),
+	// Map synthesizer mode buttons
+	contents: $.map(function(name, index) {
+		return new SynthButton(name, {left: index*80});     	
 	}),	
-}})
+}});
 
 // Keyboard container template
 var Keyboard = Container.template(function($) { return {
-    left: 0, right: 0, top: 0, bottom: 0, active: true, multipleTouch: true, skin: KeyBoardSkin,
-    behavior: Behavior({
-        // Hit based on x-y coords
-        hitIndex: function(container, x, y) {
-            y -= container.y;
-            if ((2 <= y) && (y < 150)) {
-            	if( y < 90) {    
-            		x -= container.x;
-		            // Black key hitbox
-		            if( 30 < x && x < 50) return 8; 
-		            if( 70 < x && x < 90) return 9;
-		            if( 150 < x && x < 170) return 10;
-		            if( 190 < x && x < 210) return 11;
-		            if( 230 < x && x < 250) return 12;
-		            if( 300 < x ) return 13;
-		            else return Math.floor(x/40);
-	            }
-	            // Else white key hitbox
-	           	else {
-	           		x -= container.x;
-	                return Math.floor(x / 40);
-	            }
-            } else return -1;
-        },
-        // Determines current touch ids, sends to update frequencies
-        hitTouch: function(container, id, current) {
-            var former = this.touches[id];
-            if (former != current) {
-                if (former >= 0)
-                    container.content(former).state = 0;
-                if (current >= 0)
-                    container.content(current).state = 1;
-                this.touches[id] = current;
-                this.updateFrequencies(container);
-            }	    
-        },
-        // Tracks analog sensor value
-        onAnalogValueChanged: function(container, result) {
-        	// Function defines how analog value(0-1) changes audio amplitude(loudness)
-            var test = (2*result+.5) *aval;
-            // Change Amplitude with analog input values 
-            Pins.invoke("/audio/setAmplitude", test);
+	left: 0, right: 0, top: 0, bottom: 0, active: true, multipleTouch: true, skin: KeyBoardSkin,
+	behavior: Behavior({
+		// Hit based on x-y coords
+		hitIndex: function(container, x, y) {
+			y -= container.y;
+			if ((2 <= y) && (y < 150)) {
+				if( y < 90) {    
+						x -= container.x;
+						// Black key hitbox
+						if( 30 < x && x < 50) return 8; 
+						if( 70 < x && x < 90) return 9;
+						if( 150 < x && x < 170) return 10;
+						 if( 190 < x && x < 210) return 11;
+						if( 230 < x && x < 250) return 12;
+						if( 300 < x ) return 13;
+						else return Math.floor(x/40);
+				} else { // white key hitbox
+					x -= container.x;
+					return Math.floor(x / 40);
+				}
+			} else return -1;
 		},
-        // Initialize array for multi touch
-        onCreate: function(container, data) {
-            this.touches = [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
-        },
-        // Fires on a touch
-        onTouchBegan: function(container, id, x, y, tick) {
-            this.hitTouch(container, id, this.hitIndex(container, x, y));
-        },
-        // Send -1 flag to hittouch
-        onTouchEnded: function(container, id, x, y, tick) {
-            this.hitTouch(container, id, -1);
-        },
-        // Fires on a touch position change
-        onTouchMoved: function(container, id, x, y, tick) {
-            this.hitTouch(container, id, this.hitIndex(container, x, y));
-        },       
-		// Runs after detecting touch(es) 
-        updateFrequencies: function(container) {
-            var frequencies = [];
-            var capped = [];                  
-            // For each key currently being touched	
-            for (var content = container.first; content; content = content.next){
-                if (content.state){           	
-                	// Assigns frequency to the active frequency array. 
-                	frequencies.push(content.behavior.frequency );
-                	// Caps complex modes to one frequency
-                	capped[0] = frequencies[0];	
-                }   
-            }
-            // Uses synthoption global variable to determine synthesizer mode
-            if(synthoption == "violin"){
-            	Pins.invoke("/audio/setFrequenciesViolin", capped);
-            }
-            else if(synthoption == "bell"){
-            	Pins.invoke("/audio/setFrequenciesBell", capped);
-            }
-            else if(synthoption == "laser"){
-            	Pins.invoke("/audio/setFrequenciesLaser", capped);
-            }
-            else {
-            	// Restrict total # of keys pressed to 5
-            	if(frequencies.length > 5){
-           			var filtered = frequencies.slice(0,5);
-            		Pins.invoke("/audio/setFrequencies", filtered);
-           		}
-           		else Pins.invoke("/audio/setFrequencies", frequencies);
-            }
-        },
-    }),
-    // Maps keys. white keys are 0-7 index, black keys after
-    contents: $.map(function(frequency, index) {
-    	if ( index < 8) return new Key(frequency, { left: index * 40 });
-    	else if ( index < 10) return new HalfKey(frequency, { left: 30 + (index-8) * 40 });
-       	else if ( index < 13) return new HalfKey(frequency, { left: 150 + (index-10) * 40 });
-       	else return new HalfKey(frequency, { left: 310});
-    }),
+		// Determines current touch ids, sends to update frequencies
+		hitTouch: function(container, id, current) {
+			var former = this.touches[id];
+			if (former != current) {
+				if (former >= 0) container.content(former).state = 0;
+				if (current >= 0) container.content(current).state = 1;
+				this.touches[id] = current;
+				this.updateFrequencies(container);
+			}	    
+		},
+		// Tracks analog sensor value
+		onAnalogValueChanged: function(container, result) {
+			// Function defines how analog value(0-1) changes audio amplitude(loudness)
+			var test = (2*result+.5) *aval;
+			// Change Amplitude with analog input values 
+			Pins.invoke("/audio/setAmplitude", test);
+		},
+		// Initialize array for multi touch
+		onCreate: function(container, data) {
+			this.touches = [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+		},
+		// Fires on a touch
+		onTouchBegan: function(container, id, x, y, tick) {
+			this.hitTouch(container, id, this.hitIndex(container, x, y));
+		},
+		// Send -1 flag to hittouch
+		onTouchEnded: function(container, id, x, y, tick) {
+			this.hitTouch(container, id, -1);
+		},
+		// Fires on a touch position change
+		onTouchMoved: function(container, id, x, y, tick) {
+			this.hitTouch(container, id, this.hitIndex(container, x, y));
+		},       
+			// Runs after detecting touch(es) 
+		updateFrequencies: function(container) {
+			var frequencies = [];
+			var capped = [];                  
+			// For each key currently being touched	
+			for (var content = container.first; content; content = content.next){
+				if (content.state){           	
+					// Assigns frequency to the active frequency array. 
+					frequencies.push(content.behavior.frequency );
+					// Caps complex modes to one frequency
+					capped[0] = frequencies[0];	
+				}   
+			}
+			// Uses synthoption global variable to determine synthesizer mode
+			if(synthoption == "violin"){
+				Pins.invoke("/audio/setFrequenciesViolin", capped);
+			}
+			else if(synthoption == "bell"){
+				Pins.invoke("/audio/setFrequenciesBell", capped);
+			}
+			else if(synthoption == "laser"){
+				Pins.invoke("/audio/setFrequenciesLaser", capped);
+			}
+			else {
+				// Restrict total # of keys pressed to 5
+				if(frequencies.length > 5){
+					var filtered = frequencies.slice(0,5);
+					Pins.invoke("/audio/setFrequencies", filtered);
+				} else Pins.invoke("/audio/setFrequencies", frequencies);
+			}
+		},
+	}),
+	// Maps keys. white keys are 0-7 index, black keys after
+	contents: $.map(function(frequency, index) {
+		if ( index < 8) return new Key(frequency, { left: index * 40 });
+		else if ( index < 10) return new HalfKey(frequency, { left: 30 + (index-8) * 40 });
+		else if ( index < 13) return new HalfKey(frequency, { left: 150 + (index-10) * 40 });
+		else return new HalfKey(frequency, { left: 310});
+	}),
 }});
 
 // ---------------------------------------------------------------------------------------------------------
@@ -224,7 +217,7 @@ var Keyboard = Container.template(function($) { return {
 // ---------------------------------------------------------------------------------------------------------
 // Construct instances of preceding templates/prototypes
 application.behavior = Behavior({
-   onLaunch: function(application,message) {       
+	onLaunch: function(application,message) {       
 		// Configures Audio, and analog pins on start
 		Pins.configure({
 			audio: {
@@ -235,33 +228,32 @@ application.behavior = Behavior({
 					speaker: {sampleRate: 4000, amplitude: aval}
 				},
 			},
-            analogSensor: {
-			    require: "Analog",
-			    pins: {
+			analogSensor: {
+				require: "Analog",
+				pins: {
 					analog: { pin: 54 }
-		      	} 
-		    }
+				} 
+			}
 		}, success => this.onPinsConfigured(application, success));
-    },
+	},
 	onPinsConfigured(application, success) {		
 		if (success) {
-	        /* Use the initialized analogSensor object and repeatedly 
-		   	call its read method with a given interval(20ms).  */
-		   	Pins.repeat("/analogSensor/read", 20, result => application.distribute( "onAnalogValueChanged", result ));
-
+		/* Use the initialized analogSensor object and repeatedly 
+			call its read method with a given interval(20ms).  */
+			Pins.repeat("/analogSensor/read", 20, result => application.distribute( "onAnalogValueChanged", result ));
 			// Initialized keyboard, data passed are notes C,D,E,F,G,A,B,C scale, plus all half steps added in order at the end.
 			application.add(new Keyboard([523, 587, 660, 698, 783, 880, 988, 1046, 554, 622, 740, 831, 932, 1109]));
 			// Initialized buttons to the variants defined in theme.xml
-	        application.add(new ButtonTray([0,1,2,3]));   
-	          
-	        // Starts audio output hardware 
-		   	Pins.invoke("/audio/start");
-
+			application.add(new ButtonTray([0,1,2,3]));   
+			
+			// Starts audio output hardware 
+			Pins.invoke("/audio/start");
+			
 			Pins.share("ws", {zeroconf: true, name: "simplesynth"});
 		}
 		else {
-            application.skin = new Skin({ fill:"#f78e0f" });
-            application.add(new Label({ left:0, right:0, style:errorStyle, string:"Error" }));
+			application.skin = new Skin({ fill:"#f78e0f" });
+			application.add(new Label({ left:0, right:0, style:errorStyle, string:"Error" }));
 		}
 	}
 });
