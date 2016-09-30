e6a7ed00065da0955dc1afff19a4999d19231d8d
Lizzie Prader <lprader@marvell.com>
2016-09-30 13:43:52 -0700

    Tri-color LED update

--- pwm-tri-color-led/device/led.js
+++ pwm-tri-color-led/device/led.js
@@ -1,38 +1,38 @@
 //@module
 /*
-  Copyright 2011-2014 Marvell Semiconductor, Inc.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
+ *     Copyright (C) 2010-2016 Marvell International Ltd.
+ *     Copyright (C) 2002-2010 Kinoma, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 exports.pins = {
-    red: { type: "PWM", value: 1 },
-    green: { type: "PWM", value: 1  },
-    blue: { type: "PWM", value: 1  },
-    anode: { type: "Digital", direction: "output", value: 1  } 
+    red: { type: "PWM" },
+    green: { type: "PWM"  },
+    blue: { type: "PWM"   },
+    anode: { type: "Digital", direction: "output"  } 
 };
 
-exports.configure = function( led ) {
+exports.configure = function( ) {
 	this.red.init();
 	this.green.init();
 	this.blue.init();
 	this.anode.init();
 	
 	//turn off the lights
-	this.red.write( led.pins.red.value );
-	this.green.write( led.pins.green.value );
-	this.blue.write( led.pins.blue.value );
-	this.anode.write( led.pins.anode.value );
+	this.red.write( 1 );
+	this.green.write( 1 );
+	this.blue.write( 1 );
+	this.anode.write( 1 );
 }
 
 exports.write = function( parameters ) {
@@ -49,17 +49,17 @@ exports.write = function( parameters ) {
 	}
 }
 
-exports.close = function( led ){
+exports.close = function( ){
+	//turn off the lights
+	this.red.write( 1 );
+	this.green.write( 1 );
+	this.blue.write( 1 );
+	this.anode.write( 0 );
+	
 	this.red.close();
 	this.green.close();
 	this.blue.close();
 	this.anode.close();
-	
-	//turn off the lights
-	this.red.write( led.pins.red.value );
-	this.green.write( led.pins.green.value );
-	this.blue.write( led.pins.blue.value );
-	this.anode.write( led.pins.anode.value );
 }
 
 exports.metadata = {
